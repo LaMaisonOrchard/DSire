ef2a2b8d2b518673868139d3a3939a36187c3b26
DavidO <david@errol.org.uk>
2015-01-16 20:47:59 +0000

    Implement the initial basic commands.

--- lib/dsh.d
+++ lib/dsh.d
@@ -2,6 +2,9 @@ import std.stdio;
 import std.container;
 import std.ascii;  // ASCII support
 //import std.uni;  // Unicode support
+import std.process; 
+import std.conv; 
+import std.outbuffer; 
 
 
 public class Dsh
@@ -12,15 +15,20 @@ public class Dsh
   //
   this(File out_fp, File err_fp, string[string] env)
   {
+    this.out_fp  = out_fp;
+    this.err_fp  = err_fp;
+    this.env     = env;
   }
   
   /////////////////////////////////////////////////////
   //
-  // Create a dsh interpreter where standard output is
-  // returned as a string.
+  // Create a dsh interpreter
   //
-  this(inout string out_fp, File err_fp, string[string] env)
+  this(OutBuffer out_buf, File err_fp, string[string] env)
   {
+    this.out_buf = out_buf;
+    this.err_fp  = err_fp;
+    this.env     = env;
   }
   
   /////////////////////////////////////////////////////
@@ -31,22 +39,15 @@ public class Dsh
   //
   bool run(const(char)[] command)
   {
-    enum state
-    {
-      SPACE,
-      ARG,
-      DOUBLE_QUOTE,
-      BACK_QUOTE
-    };
     
     auto parseState = state.SPACE;
     
-    char[]   entry;
-    string[] line;
-    bool     more = true;
+    char[]    entry;
+    Element[] line;
+    bool      more = true;
     
     // Create a stack of input sources
-    auto inputs = new SList!(const (char)[])();
+    auto inputs = new SList!(const(char)[])();
     inputs.insertFront(command);
     
     while (!inputs.empty())
@@ -59,16 +60,9 @@ public class Dsh
 	if (input[0] == '\r')
 	{
 	  // EOL
-	  if (entry.length > 0)
-	  {
-	    line ~= entry.idup;
-	    entry = entry[0..0];
-	  }
-	  parseState = state.SPACE;
-	    
-	  // Process the line
-	  more = process(line);
-	  line = line[0..0];
+	  more = eol(parseState, line, entry);
+	  entry = entry[0..0];
+	  line  = line [0..0];
 	  
 	  if ((input.length > 1) && (input[1] == '\n'))
 	  {
@@ -79,16 +73,9 @@ public class Dsh
 	else if (input[0] == '\n')
 	{
 	  // EOL
-	  if (entry.length > 0)
-	  {
-	    line ~= entry.idup;
-	    entry = entry[0..0];
-	  }
-	  parseState = state.SPACE;
-	    
-	  // Process the line
-	  more = process(line);
-	  line = line[0..0];
+	  more = eol(parseState, line, entry);
+	  entry = entry[0..0];
+	  line  = line [0..0];
 	}
 	else
 	{
@@ -109,24 +96,32 @@ public class Dsh
 		// Consume the start quote
 		parseState = state.BACK_QUOTE;
 	      }
+	      else if (input[0] == ';')
+	      {
+		// EOL
+		more = eol(parseState, line, entry);
+		entry = entry[0..0];
+		line  = line [0..0];
+	      }
 	      else if (input[0] == '\\')
 	      {
-		if (input.length > 1)
+		if (input.length < 2)
 		{
-		  // Illegal quote
+		  // Illegal escape sequence
 		}
 		else
 		{
 		  // Consume the escape
 		  input = input[1..$];
 		
+		  // Add to the entry
 		  entry ~= input[0];
 		  parseState  = state.ARG;		
 		}
 	      }
 	      else
 	      {
-		// Clear the entry and add to the entry
+		// Add to the entry
 		entry ~= input[0];
 		parseState  = state.ARG;
 	      }
@@ -136,14 +131,14 @@ public class Dsh
 	      if (isWhite(input[0]))
 	      {
 		// End of the entry
-		line ~= entry.idup;
+		line ~= Element(parseState, entry);
 		entry = entry[0..0];
 		parseState = state.SPACE;
 	      }
 	      else if (input[0] == '\"')
 	      {
 		// End of the entry
-		line ~= entry.idup;
+		line ~= Element(parseState, entry);
 		entry = entry[0..0];
 		
 		// Consume the start quote
@@ -152,37 +147,103 @@ public class Dsh
 	      else if (input[0] == '`')
 	      {
 		// End of the entry
-		line ~= entry.idup;
+		line ~= Element(parseState, entry);
 		entry = entry[0..0];
 		
 		// Consume the start quote
 		parseState = state.BACK_QUOTE;
 	      }
+	      else if (input[0] == ';')
+	      {
+		// EOL
+		more = eol(parseState, line, entry);
+		entry = entry[0..0];
+		line  = line [0..0];
+	      }
 	      else if (input[0] == '\\')
 	      {
-		if (input.length > 1)
+		if (input.length < 2)
 		{
-		  // Illegal quote
+		  // Illegal escape sequence
 		}
 		else
 		{
 		  // Consume the escape
 		  input = input[1..$];
 		
+		  // Add to the entry
 		  entry ~= input[0];
 		}
 	      }
 	      else
 	      {
-		// Clear the entry and add to the entry
+		// Add to the entry
 		entry ~= input[0];
 	      }
 	      break;
 	      
 	    case state.DOUBLE_QUOTE: // quote
+	      if (input[0] == '\"')
+	      {
+		// End of the entry
+		line ~= Element(parseState, entry);
+		entry = entry[0..0];
+		
+		// Consume the end quote
+		parseState = state.SPACE;
+	      }
+	      else if (input[0] == '\\')
+	      {
+		if (input.length < 2)
+		{
+		  // Illegal escape sequence
+		}
+		else
+		{
+		  // Consume the escape
+		  input = input[1..$];
+		
+		  // Add to the entry
+		  entry ~= input[0];
+		}
+	      }
+	      else
+	      {
+		// Add to the entry
+		entry ~= input[0];
+	      }
 	      break;
 	      
 	    case state.BACK_QUOTE: // operation
+	      if (input[0] == '`')
+	      {
+		// End of the entry
+		line ~= Element(parseState, entry);
+		entry = entry[0..0];
+		
+		// Consume the end quote
+		parseState = state.SPACE;
+	      }
+	      else if (input[0] == '\\')
+	      {
+		if (input.length < 2)
+		{
+		  // Illegal escape sequence
+		}
+		else
+		{
+		  // Consume the escape
+		  input = input[1..$];
+		
+		  // Add to the entry
+		  entry ~= input[0];
+		}
+	      }
+	      else
+	      {
+		// Add to the entry
+		entry ~= input[0];
+	      }
 	      break;
 	      
 	    default:
@@ -197,38 +258,223 @@ public class Dsh
     }
     
     // EOL
-    if (entry.length > 0)
-    {
-      line ~= entry.idup;
-      entry = entry[0..0];
-    }
-    
-    // Process the line
-    more = process(line);
-    line = line[0..0];
+    more = eol(parseState, line, entry);
+    entry = entry[0..0];
+    line  = line [0..0];
     
     return more;
   }
   
   /////////////////////////////////////////////////////
   //
-  // Process shell command
+  // Process the end of a command
+  //
+  // RETURN Returns true if more commands should be processed.
+  //
+  private bool eol(ref state parseState, Element[] line, const(char)[] entry)
+  {
+    bool more = true;
+    
+    if (parseState != state.SPACE)
+    {
+      if (entry.length > 0)
+      {
+	line ~= Element(parseState, entry);
+      }
+      parseState = state.SPACE;
+    }
+	
+    // Process the line
+    more = process(line);
+    
+    return more;
+  }
+  
+  // This is the current state of the parsing engine.
+  private enum state
+  {
+    SPACE,             // Parsing white space
+    ARG,               // Parsing a simple argument
+    DOUBLE_QUOTE,      // Parsing a quoted argument
+    BACK_QUOTE         // Parsing a back quoted argument
+  };
+  
+  
+  /////////////////////////////////////////////////////
+  //
+  // This structure represents a parsed element on a line.
+  // This is the element in its unexpanded state. This
+  // structure will expand the element when requested.
+  //
+  private struct Element
+  {
+    this(state type, const(char)[] arg)
+    {
+      this.type = type;
+      this.arg  = arg.idup;
+    }
+    
+    /////////////////////////////////////////////////////
+    //
+    // Expand the element and return the element in its
+    // expanded form
+    //
+    // RETURN False is returned in the shell exits
+    //
+    const(char)[][] expand()
+    {
+      const(char)[] rtn;
+      
+      rtn = expandVariables(this.arg);
+      
+      switch(type)
+      {
+	default:
+	  return [""];  // Should never get here
+	  
+	case state.ARG:      
+	  return [rtn];
+	  
+	case state.DOUBLE_QUOTE:      
+	  return [rtn];
+	  
+	case state.BACK_QUOTE:      
+	  return [rtn];
+      }
+    }
+    
+    const(char)[]expandVariables(const(char)[] arg)
+    {
+      // STUB
+      return  arg;
+    }
+    
+    state  type;
+    string arg;
+  };
+  
+  /////////////////////////////////////////////////////
+  //
+  // Process a hell command
   //
   // RETURN False is returned in the shell exits
   //
-  private bool process(string[] line)
+  private bool process(Element[] line)
   {
-    foreach(string entry; line)
+    bool more = true;
+    
+    const(char)[][] expanded;
+    
+    // Expand all the elements into their final form
+    foreach(Element entry; line)
     {
-      writeln(entry);
+      expanded ~= entry.expand();
     }
     
-    return true;
+    // Is there anything to process
+    if (expanded.length > 0)
+    {
+      // Process the command
+      switch (expanded[0])
+      {
+	case "exit":
+	  more = false;
+	  if (expanded.length > 1)
+	  {
+	    try
+	    {
+	      exitStatus = to!int(expanded[1]);
+	    }
+	    catch(Exception)
+	    {
+	      // Bad conversion
+	      err_fp.writeln("Bad argument to exit : ", expanded[1]);
+	      exitStatus = -1;
+	    }
+	  }
+	  else
+	  {
+	    // The default is no error
+	    exitStatus = 0;
+	  }
+	  break;
+	  
+	case "echo":
+	  const(char)[][] items = expanded[1..$];
+	  
+	  if (out_buf !is null)
+	  {
+	    if (items.length > 0)
+	    {	
+	      out_buf.write(items[0]);
+	      foreach(const(char)[] item; items[1..$])
+	      {
+		out_buf.write(" ");
+		out_buf.write(item);
+	      }
+	    }
+	  }
+	  else
+	  {
+	    if (items.length > 0)
+	    {	
+	      out_fp.write(items[0]);
+	      foreach(const(char)[] item; items[1..$])
+	      {
+		out_fp.write(" ");
+		out_fp.write(item);
+	      }
+	    }
+	    out_fp.writeln();
+	  }
+	  
+	  // No errors
+	  exitStatus = 0;
+	  break;
+	  
+	default:
+	  // Excute the command as a program
+	  try
+	  {
+	    if (out_buf !is null)
+	    {
+	      // Use a pipe to capture stdout as text to be processed
+	      
+	      char[] buffer;
+	      auto pipes = pipeProcess(expanded, Redirect.stdout);
+	      scope(exit) wait(pipes.pid);
+	      
+	      while (0 < pipes.stdout.readln(buffer))
+	      {
+		out_buf.write(buffer);
+	      }
+	    }
+	    else
+	    {
+	      // Run the program passing on the stdout
+	      auto pid   = spawnProcess(expanded, stdin, out_fp, err_fp, env);
+	      exitStatus = wait(pid);
+	    }
+	  }
+	  catch(Exception ex)
+	  {
+	    // Report and errors thrown by the process
+	    err_fp.writeln(ex.msg);
+	  }
+	  break;
+      }
+    }
+    
+    return more;
   }
   
   int ExitStatus()
   {
-    return 0;
+    return exitStatus;
   }
-  
+  OutBuffer      out_buf;
+  File           out_fp;
+  File           err_fp;
+  string[string] env;
+  int            exitStatus = 0;
 }
---
+++ test.sh
@@ -0,0 +1,5 @@
+
+echo fred bill "a  b"
+ls *.d
+exit 7
+echo harry
